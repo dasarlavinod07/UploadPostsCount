ffc1f22008b7066d1c6842179f4e6231829b791c
Vinod Dasarla <vinod.dasarla@siemens.com>
2022-12-09 20:49:31 +0530

    Assignment Implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter,Output} from '@angular/core';
+import { Post } from './posts';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'UploadPostsCount';
+export class AppComponent
+ { 
+  postCount=0;
+
+  posts: Post[] = []
+
+  title: string = '';
+  content: string = '';  
+  
+  upload()
+  {
+    this.posts.push({title:this.title,content:this.content});
+    this.postCount++;
+  }
 }
